3af0e2d25a75cf847dc04c3650ed235a2ee2134f
Filip <filipradovic05@gmail.com>
2020-01-11 18:18:40 +0100

    MenuScene update 3

--- ASCIIRacer/include/Scenes/MenuScene.hpp
+++ ASCIIRacer/include/Scenes/MenuScene.hpp
@@ -1,11 +1,18 @@
 #pragma once
 #include "Scenes/Scene.hpp"
+#include "windows.h"
+#include <utility>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
 
 class MenuScene : public Scene {
 	protected:
 		void fetchOptions();
 		void drawMenu();
 		void moveCursor(bool down);
+		void addOption(string name, ptr_Scene s);
 	public:
 		void onStart() override;
 		void onLoop() override;
